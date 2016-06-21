a5670dc1d5a12ad97d9ec4dd03126b62ddf0d8ba
Matthew Denson <mdenson@dayspring-tech.com>
2016-06-20 18:02:17 -0700

    Rough fill in of the Timesheet http service.

--- src/app/services/timesheet.service.ts
+++ src/app/services/timesheet.service.ts
@@ -1,6 +1,46 @@
 import {Injectable} from '@angular/core';
 
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
+
+import { DayInfo } from '../models';
+
 @Injectable()
 export class TimesheetService {
-   constructor() { }
+    private _week$: Subject<DayInfo[]>;
+    private baseUrl: string;
+    private dataStore: {  // This is where we will store our data in memory
+        week: DayInfo[]
+    };
+
+    // Using Angular DI we use the HTTP service
+    constructor(private http: Http) {
+        this.baseUrl = 'http://malachi/timesheetPHP/staff/staff_json.php';
+        this.dataStore = { week: [] };
+        this._week$ = <Subject<DayInfo[]>>new Subject();
+    }
+
+    get week$() {
+        return this._week$.asObservable();
+    }
+
+    loadTimeData(today: Date, staff: string) {
+        let todaystr = this._isoDate(today);
+        let url = this.baseUrl + '?staff=' + staff + '&today=' + todaystr;
+        this.http.get(url)
+            .map(response => response.json())
+            .subscribe(data => {
+                this.dataStore.week = data;
+                this._week$.next(this.dataStore.week);
+            }, error => console.log('Could not load week.'));
+    }
+
+    private _isoDate(d: Date) {
+        function pad(s: number): String {
+            return (s < 10) ? '0${s}' : '${s}';
+        }
+        return [d.getFullYear(), pad(d.getMonth() + 1), pad(d.getDate())].join('-');
+    }
 }
