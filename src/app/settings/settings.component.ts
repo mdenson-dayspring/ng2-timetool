9cf15a4d1548e9ea396914ac28498b3dc23865b7
Matthew Denson <mdenson@dayspring-tech.com>
2016-06-24 12:27:25 -0700

    Reformatting

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AppState } from '../services';
+import { Context } from '../models';
 
 @Component({
-    template: `
-        <p>Settings Page</p>
-    `,
+    template: require('app/settings/settings.component.html'),
     selector: 'settings'
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
+    private _context: Context;
+
     constructor(private _appState: AppState) { }
+
+    ngOnInit() {
+        this._context = this._appState.getContext();
+        this._appState.context$.subscribe(updated => {
+            this._context = updated;
+        });
+
+    }
+
 }
