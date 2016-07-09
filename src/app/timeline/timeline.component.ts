173b039a36ca0afdf32c8fc08f3c0e5df8d04bfc
Matthew Denson <mdenson@dayspring-tech.com>
2016-07-08 18:54:38 -0700

    Added day progress bar

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { Context } from '../models';
+import { Context, HM } from '../models';
 import { AppState } from '../services';
 
 @Component({
@@ -9,12 +9,50 @@ import { AppState } from '../services';
 })
 export class TimelineComponent implements OnInit {
     context: Context;
+    am: string;
+    lunch: string;
+    pm: string;
     constructor(private _appState: AppState) { }
 
     ngOnInit() {
-        this.context = this._appState.getContext();
+        this._setup(this._appState.getContext());
         this._appState.context$.subscribe(updated => {
-            this.context = updated;
+            this._setup(updated);
         });
+    }
+
+    private _setup( context: Context) {
+        this.context = context;
+
+        let amPerc: number = 0;
+        let lunchPerc: number = 0;
+        let pmPerc: number = 0;
+        let noon = new HM('12:00');
+        let afterLunch = noon.add(context.today.lunch);
+        let noonMinutes = noon.sub(context.today.arrive);
+
+        let totalMin = context.today.hours.minutes;
+        if (context.now.leave.minutes < noon.minutes) {
+            console.log('am');
+            amPerc = Math.floor( context.now.hoursLessLunch.minutes / totalMin * 100);
+        } else if (context.now.leave.minutes < afterLunch.minutes) {
+            console.log('lunch');
+            amPerc = Math.floor(noon.minutes / totalMin * 100);
+            let lunch = context.now.hoursLessLunch.sub(noonMinutes).minutes;
+            lunchPerc = Math.floor(lunch / totalMin * 100);
+        } else {
+            console.log('pm');
+            amPerc = Math.floor(noonMinutes.minutes / totalMin * 100);
+            lunchPerc = Math.floor(context.now.lunch.minutes / totalMin * 100);
+            let pm = context.now.hoursLessLunch.sub(noonMinutes).minutes;
+            pmPerc = Math.floor(pm / totalMin * 100);
+        }
+        console.log(amPerc, lunchPerc, pmPerc);
+        if (amPerc + lunchPerc  + pmPerc> 100) {
+            pmPerc = 100 - (amPerc + lunchPerc);
+        }
+        this.am = '' + amPerc + '%';
+        this.lunch = '' + lunchPerc + '%';
+        this.pm = '' + pmPerc + '%';
     }
 }
